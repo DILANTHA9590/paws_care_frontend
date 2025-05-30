c90d74fb806c78b78ff9edbecde2c3a3e6d31b4a
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-30 13:05:18 +0530

    implement basic image slider with left and right controls🌟✅

--- src/home_main/pages/HomePage.jsx
+++ src/home_main/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
-
+import { FaChevronCircleLeft } from "react-icons/fa";
+import { FaChevronCircleRight } from "react-icons/fa";
 export default function HomePage() {
   const myimages = ["headerimage1.avif", "headerimage2.jpg"];
   return (
@@ -9,15 +10,19 @@ export default function HomePage() {
         className="bg-cover bg-center h-full flex flex-col items-center justify-center relative"
         style={{ backgroundImage: `url(/${myimages[1]})` }}
       >
-        <div className="bg-black/10  w-full h-full bottom-0 absolute "></div>\
-        <div className="flex">
-          <h1 className="text-5xl">left </h1>
-          <h1 className="text-5xl md:text-6xl font-extrabold text-center text-white leading-tight">
+        <div className="bg-black/10  w-full h-full bottom-0 absolute "></div>
+        <div className="flex items-center justify-between w-full gap-x-5">
+          <h1 className="text-5xl text-white sm:p-4">
+            <FaChevronCircleLeft />{" "}
+          </h1>
+          <h1 className="text-4xl  md:text-6xl font-extrabold text-center text-white leading-tight">
             Welcome to <span className="">HappyPaws Care</span>
             <br />
             <span className="text-white">Your Pet’s Second Home</span>
           </h1>
-          <h1 className="text-5xl">left </h1>
+          <h1 className="text-5xl text-white sm:p-4">
+            <FaChevronCircleRight />
+          </h1>
         </div>
         {/* <Link className="inline-block bg-green-400 p-6 font-bold"></Link> */}
         <div className="flex absolute bottom-0 gap-1 p-3 ">
