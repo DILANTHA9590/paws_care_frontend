42965d7bd7e86eda9765d6f1f1a4f0538975204d
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-30 12:49:47 +0530

    add header images to public folder✅🌟

--- src/home_main/HomeMain.jsx
+++ src/home_main/HomeMain.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
 export default function HomeMain() {
   return (
     <>
       <div className="min-h-screen bg-amber-400">
         <div className="h-[12vh] bg-amber-500">I am nav</div>
-        <div>
+        <div className="h-[calc(100vh-12vh)]">
           <Routes>
-            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="contact" element={<h1>ContactUs</h1>} />
             <Route path="pet foods" element={<h1>pet foods</h1>} />
           </Routes>
