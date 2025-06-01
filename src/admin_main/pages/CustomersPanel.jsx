d080a503bcb8e839097fae3f9dd2625b99a2ef14
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-01 06:11:05 +0530

    create admin customer panel🔗🌟

---
+++ src/admin_main/pages/CustomersPanel.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+export default function CustomersPanel() {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1>Cistomer panel</h1>
+    </>
+  );
+}
