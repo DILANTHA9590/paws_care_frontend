e58e855f39336dec471594549b6b5f87dca3bbe8
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-01 21:45:43 +0530

    Create basic Admin Dashboard with personalized greeting using user state

---
+++ src/admin_main/pages/AdminDashBoard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useLocation } from "react-router";
+import { motion } from "framer-motion";
+
+export default function AdminDashBoard() {
+  const location = useLocation();
+  console.log(location.state);
+  const { email, firstName, lastName } = location.state;
+
+  return (
+    <>
+      <div>
+        <motion.h1
+          className="text-4xl font-bold bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-transparent bg-clip-text"
+          initial={{
+            opacity: 0,
+          }}
+          animate={{ opacity: 4, y: 0 }}
+          transition={{
+            duration: 5,
+          }}
+        >
+          Hi Admin{" "}
+          {`${
+            firstName.charAt(0).toUpperCase() + firstName.slice(1)
+          } ${lastName}`}
+          , your dashboard is ready!
+          <span className="text-black">🚀</span>
+        </motion.h1>
+      </div>
+    </>
+  );
+}
