2af8b229a8c54cf84d7daae02d94bde2fa50d8fb
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-06 22:25:05 +0530

    create update user component✅🌟

---
+++ src/admin_main/components/UpdateUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useLocation } from "react-router";
+
+export default function UpdateUser() {
+  const location = useLocation();
+
+  return (
+    <>
+      <div>UpdateUser</div>
+    </>
+  );
+}
