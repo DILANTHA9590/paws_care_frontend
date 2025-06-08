d20b0d9e19f572655984946aae1950556693001a
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-08 23:50:49 +0530

    import petsupport section out home page🎨✅

--- src/home_main/pages/HomePage.jsx
+++ src/home_main/pages/HomePage.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router";
 import { FaChevronCircleLeft } from "react-icons/fa";
 import { FaChevronCircleRight } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa6";
-import { motion, AnimatePresence, delay } from "framer-motion";
+import { motion, AnimatePresence, delay, color } from "framer-motion";
 import { ImWhatsapp } from "react-icons/im";
 import { FaInstagram } from "react-icons/fa";
 import Header from "../component/Header";
 import { PiDogThin } from "react-icons/pi";
 import OurServices from "../component/OurServices";
 import { div } from "framer-motion/client";
+import PetSupportSection from "../component/PetSupportSection";
 export default function HomePage() {
   const [index, setIndex] = useState(0);
 
@@ -25,10 +26,7 @@ export default function HomePage() {
 
       {/* Main container for the Services section */}
       <OurServices />
-      <div
-        className="w-full h-[70vh] bg-cover bg-center mt-6 ba"
-        style={{ backgroundImage: "url('/mysection1.jpg')" }}
-      ></div>
+      <PetSupportSection />
     </>
   );
 }
