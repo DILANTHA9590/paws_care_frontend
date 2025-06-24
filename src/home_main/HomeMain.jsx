e5d10ae3d564aab23593bc06e86886b13e76169c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-24 23:26:54 +0530

    add global cart count context using CountContext

--- src/home_main/HomeMain.jsx
+++ src/home_main/HomeMain.jsx
@@ -13,9 +13,11 @@ import DoctorOverview from "./pages/DoctorOverview";
 import DoctorBookingForm from "./component/DoctorBookingForm";
 import ProductsPage from "./pages/ProductsPage";
 import ProductOverView from "./pages/ProductOverView";
+import { CountContext } from "../utills/context/countContext";
 export default function HomeMain() {
   const [token, setToken] = useState(true);
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [count, setCount] = useState(1);
   useEffect(() => {
     const token = localStorage.getItem("token");
     setToken(token);
@@ -24,24 +26,26 @@ export default function HomeMain() {
   }, []);
   return (
     <>
-      <div className="min-h-screen">
-        <DeskTopNavbar />
+      <CountContext.Provider value={{ count, setCount }}>
+        <div className="min-h-screen">
+          <DeskTopNavbar />
 
-        <div className="h-[calc(100vh-12vh)]">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="contact" element={<h1>ContactUs</h1>} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="pet foods" element={<h1>pet foods</h1>} />
-            <Route path="bookdoctor" element={<BookDoctor />} />
+          <div className="h-[calc(100vh-12vh)]">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="contact" element={<h1>ContactUs</h1>} />
+              <Route path="about" element={<AboutUs />} />
+              <Route path="pet foods" element={<h1>pet foods</h1>} />
+              <Route path="bookdoctor" element={<BookDoctor />} />
 
-            <Route path="book-doctor" element={<DoctorBookingForm />} />
-            <Route path="doctor/:id" element={<DoctorOverview />} />
-            <Route path="products" element={<ProductsPage />} />
-            <Route path="product/:id" element={<ProductOverView />} />
-          </Routes>
+              <Route path="book-doctor" element={<DoctorBookingForm />} />
+              <Route path="doctor/:id" element={<DoctorOverview />} />
+              <Route path="products" element={<ProductsPage />} />
+              <Route path="product/:id" element={<ProductOverView />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </CountContext.Provider>
     </>
   );
 }
