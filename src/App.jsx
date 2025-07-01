a4b73e50f9429c926800ea9718207118d1ef5c2e
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-01 22:48:15 +0530

    ⬆️ Add Stripe Elements provider wrapping the app for payment integration

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,25 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "react-hot-toast";
 import AdminHome from "./admin_main/AdminHome";
 import LoginForm from "./LoginForm";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51RSFLcP2O6eKPEmTYzM7SuHBXwwjcSDhD4S8E1kKiA2S6TSiuYZKJcPrctaGfcL4UX1IzqkiKwvr5ffaWzaqBXiN00R5Ne6Qug"
+);
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Toaster />
-        <Routes>
-          <Route path="/*" element={<HomeMain />}></Route>
-          <Route path="/admin/*" element={<AdminHome />}></Route>
-          <Route path="/login" element={<LoginForm />}></Route>
-          <Route path="/signnup" element={<HomeMain />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Elements stripe={stripePromise}>
+        <BrowserRouter>
+          <Toaster />
+          <Routes>
+            <Route path="/*" element={<HomeMain />}></Route>
+            <Route path="/admin/*" element={<AdminHome />}></Route>
+            <Route path="/login" element={<LoginForm />}></Route>
+            <Route path="/signnup" element={<HomeMain />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Elements>
     </>
   );
 }
