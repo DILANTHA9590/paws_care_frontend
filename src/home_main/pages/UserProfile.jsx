c66b44ad67b36b7c92a0cca98183e0dc066fad76
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-03 13:43:07 +0530

    create user profile component

---
+++ src/home_main/pages/UserProfile.jsx
@@ -0,0 +1,12 @@
+import { div } from "framer-motion/client";
+import React from "react";
+
+export default function UserProfile() {
+  return (
+    <div className="">
+      <div className=" w-[30%] bg-amber-500"></div>
+
+      <div className="w-[70%] bg-amber-800"></div>
+    </div>
+  );
+}
