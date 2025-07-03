613e54f6327160cc54dd54268b4ee34c8ce93628
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-03 13:39:33 +0530

    wrap entire app with TokenContext provider for global token state

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,27 @@ import AdminHome from "./admin_main/AdminHome";
 import LoginForm from "./LoginForm";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { TokenContext } from "./utills/context/countContext";
 const stripePromise = loadStripe(
   "pk_test_51RSFLcP2O6eKPEmTYzM7SuHBXwwjcSDhD4S8E1kKiA2S6TSiuYZKJcPrctaGfcL4UX1IzqkiKwvr5ffaWzaqBXiN00R5Ne6Qug"
 );
 function App() {
+  const [token, setToken] = useState();
   return (
     <>
-      <Elements stripe={stripePromise}>
-        <BrowserRouter>
-          <Toaster />
-          <Routes>
-            <Route path="/*" element={<HomeMain />}></Route>
-            <Route path="/admin/*" element={<AdminHome />}></Route>
-            <Route path="/login" element={<LoginForm />}></Route>
-            <Route path="/signnup" element={<HomeMain />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </Elements>
+      <TokenContext.Provider value={{ token, setToken }}>
+        <Elements stripe={stripePromise}>
+          <BrowserRouter>
+            <Toaster />
+            <Routes>
+              <Route path="/*" element={<HomeMain />}></Route>
+              <Route path="/admin/*" element={<AdminHome />}></Route>
+              <Route path="/login" element={<LoginForm />}></Route>
+              <Route path="/signnup" element={<HomeMain />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </Elements>
+      </TokenContext.Provider>
     </>
   );
 }
