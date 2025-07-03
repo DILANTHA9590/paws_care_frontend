fa5979928885bd55bb53ae15d9c9fd4b1733001d
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-03 21:58:17 +0530

    ♻️ Keep token in state from localStorage to prevent loss on page reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -15,6 +15,13 @@ const stripePromise = loadStripe(
 );
 function App() {
   const [token, setToken] = useState();
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    if (savedToken) {
+      setToken(savedToken);
+    }
+  }, []);
+
   return (
     <>
       <TokenContext.Provider value={{ token, setToken }}>
