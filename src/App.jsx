c749c1affa0819cb299f4f0c5cd1107711e953ba
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-08 17:40:25 +0530

    wrap App with ImageContext.Provider to manage user image globally🟢🌟

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,30 @@ import AdminHome from "./admin_main/AdminHome";
 import LoginForm from "./LoginForm";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import { TokenContext } from "./utills/context/countContext";
+import { ImageContext, TokenContext } from "./utills/context/countContext";
+import { image } from "framer-motion/client";
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 function App() {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
+  const [image, setImage] = useState();
 
   return (
     <>
-      <TokenContext.Provider value={{ token, setToken }}>
-        <Elements stripe={stripePromise}>
-          <BrowserRouter>
-            <Toaster />
-            <Routes>
-              <Route path="/*" element={<HomeMain />}></Route>
-              <Route path="/admin/*" element={<AdminHome />}></Route>
-              <Route path="/login" element={<LoginForm />}></Route>
-              <Route path="/signnup" element={<HomeMain />}></Route>
-            </Routes>
-          </BrowserRouter>
-        </Elements>
-      </TokenContext.Provider>
+      <ImageContext.Provider value={{ image, setImage }}>
+        <TokenContext.Provider value={{ token, setToken }}>
+          <Elements stripe={stripePromise}>
+            <BrowserRouter>
+              <Toaster />
+              <Routes>
+                <Route path="/*" element={<HomeMain />}></Route>
+                <Route path="/admin/*" element={<AdminHome />}></Route>
+                <Route path="/login" element={<LoginForm />}></Route>
+                <Route path="/signnup" element={<HomeMain />}></Route>
+              </Routes>
+            </BrowserRouter>
+          </Elements>
+        </TokenContext.Provider>
+      </ImageContext.Provider>
     </>
   );
 }
