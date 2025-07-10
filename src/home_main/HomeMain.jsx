14bdafd7e79358b57f651fdb63b267da1f2bf291
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-10 22:43:31 +0530

    🚪 Update HomeMain: change doctor route from doctor/:id to doctors/:id

--- src/home_main/HomeMain.jsx
+++ src/home_main/HomeMain.jsx
@@ -42,7 +42,7 @@ export default function HomeMain() {
               <Route path="pet foods" element={<h1>pet foods</h1>} />
               <Route path="bookdoctor" element={<BookDoctor />} />
               <Route path="book-doctor" element={<DoctorBookingForm />} />
-              <Route path="doctor/:id" element={<DoctorOverview />} />
+              <Route path="doctors/:id" element={<DoctorOverview />} />
               <Route path="products" element={<ProductsPage />} />
               <Route path="product/:id" element={<ProductOverView />} />
               <Route path="cart" element={<CartDetails />} />
