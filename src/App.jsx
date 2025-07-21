69c594af8fd6dc4f5d58436b25431d883debce26
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-21 12:03:32 +0530

    import react for  app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
